2b048b8d54216e8b97881b182346e74116d04681
mbohun <martin.bohun@gmail.com>
2014-07-29 12:02:05 +1000

    cleanup, removed/merged duplicate-s

--- code.google-issues-2-github-issues/githubapi-get-all-repos.sh
+++ code.google-issues-2-github-issues/githubapi-get-all-repos.sh
@@ -14,22 +14,24 @@ temp=`basename $0`
 TMPFILE=`mktemp /tmp/${temp}.XXXXXX` || exit 1
 
 # single page result-s (no pagination), have no Link: section, the grep result is empty
-last_page=`curl -s -I "https://api.github.com/users/$1/repos" | grep '^Link:' | sed -e 's/^Link:.*page=//g' | sed -e 's/>.*$//g'`
+last_page=`curl -s -I "https://api.github.com/users/$1/repos" | grep '^Link:'`
 sleep $GITHUB_REQUEST_RATE
 
 # does this result use pagination?
 if [ -z "$last_page" ]; then
     # no - this result has only one page
-    curl -s -i "https://api.github.com/users/$1/repos" | grep '"name":' | sed -e 's/^.*name"://g' >> $TMPFILE;
+    curl -s -i "https://api.github.com/users/$1/repos" | grep '"name":' >> $TMPFILE;
 
 else
-    # yes - this result is on multiple pages
+    # yes - this result is on multiple pages; extract the last_page number
+    last_page=`echo $last_page | sed -e 's/^Link:.*page=//g' | sed -e 's/>.*$//g'`
+
     p=1
     while [ "$p" -le "$last_page" ]; do
-	curl -s -i "https://api.github.com/users/$1/repos?page=$p" | grep '"name":' | sed -e 's/^.*name"://g' >> $TMPFILE
+	curl -s -i "https://api.github.com/users/$1/repos?page=$p" | grep '"name":' >> $TMPFILE
 	p=$(($p + 1))
 	sleep $GITHUB_REQUEST_RATE
     done
 fi
 
-cat $TMPFILE | sed -e 's/^ "//g' | sed -e 's/",$//g'| sort
+cat $TMPFILE | sed -e 's/^.*name"://g' | sed -e 's/^ "//g' | sed -e 's/",$//g'| sort
