25c6a82b599263c6bc5ca8fbfc131ea2c4cb1663
mbohun <martin.bohun@gmail.com>
2014-07-29 11:02:27 +1000

    handle correctly BOTH single AND multi-page result-s

--- code.google-issues-2-github-issues/githubapi-get-all-repos.sh
+++ code.google-issues-2-github-issues/githubapi-get-all-repos.sh
@@ -8,14 +8,22 @@ GITHUB_REQUEST_RATE=2
 temp=`basename $0`
 TMPFILE=`mktemp /tmp/${temp}.XXXXXX` || exit 1
 
-last_page=`curl -s -I "https://api.github.com/orgs/$1/repos" | grep '^Link:' | sed -e 's/^Link:.*page=//g' | sed -e 's/>.*$//g'`
+last_page=`curl -s -I "https://api.github.com/users/$1/repos" | grep '^Link:' | sed -e 's/^Link:.*page=//g' | sed -e 's/>.*$//g'`
 sleep $GITHUB_REQUEST_RATE
 
-p=1
-while [ "$p" -le "$last_page" ]; do
-    curl -s -i "https://api.github.com/orgs/$1/repos?page=$p" | grep '"name":' | sed -e 's/^.*name"://g' >> $TMPFILE
-    p=$(($p + 1))
-    sleep $GITHUB_REQUEST_RATE
-done
+# does this result use pagination?
+if [ -z "$last_page" ]; then
+    # no - this result has only one page
+    curl -s -i "https://api.github.com/users/$1/repos" | grep '"name":' | sed -e 's/^.*name"://g' >> $TMPFILE;
+
+else
+    # yes - this result is on multiple pages
+    p=1
+    while [ "$p" -le "$last_page" ]; do
+	curl -s -i "https://api.github.com/users/$1/repos?page=$p" | grep '"name":' | sed -e 's/^.*name"://g' >> $TMPFILE
+	p=$(($p + 1))
+	sleep $GITHUB_REQUEST_RATE
+    done
+fi
 
 cat $TMPFILE | sed -e 's/^ "//g' | sed -e 's/",$//g'| sort
