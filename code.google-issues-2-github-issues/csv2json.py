fe7e62799a861f9a9c9efde222998adcfd1ebdb7
mbohun <martin.bohun@gmail.com>
2014-08-12 17:34:06 +1000

    cleanup: removed the MIGRATION specific code; this will be part of the MIGRATION script; this script will be exclusively ONLY SCRAPING related; added some trivial info/progress bar to notify the user the progress of the SCRAPING

--- code.google-issues-2-github-issues/csv2json.py
+++ code.google-issues-2-github-issues/csv2json.py
@@ -63,59 +63,9 @@ def handle_element(element, result, handler_table):
     except KeyError:
         result.append({ "error" : { "text" : element.text.encode('utf8'), "error": element.tag}})
 
-# NOTE: the extraction/assignment of project label (for migration) does NOT have to be here in
-#       SCRAPING script, it should be (and it will be) moved in to the
-#       MIGRATION script
-def extract_project_labels(issue):
-    text = unicodedata.normalize('NFKD', issue["AllLabels"]).encode('ascii', 'ignore')
-    labels = text.strip().split(",")
-
-    project = []
-
-    # remove empty and non project label-s like Type-, Priority-
-    for label_text in labels:
-        label = label_text.strip()
-        if len(label) == 0:
-            continue
-
-        if label.find("Type") > -1:
-            continue
-
-        if label.find("Priority") > -1:
-            continue
-
-        if label.find("Milestone") > -1:
-            continue
-
-        project.append(label)
-
-    # TODO: do it properly/cleanly
-    # NORMALIZE - remove synonyms like SpatialPortal Spatial-Portal, replacing it with one label: SpatialPortal
-    # This applies to all Labels string: project names (synonyms, typos), priority (high, High, HIgh), etc.
-    if "Spatial-Portal" in project:
-        project.pop(project.index("Spatial-Portal"))
-        if not "SpatialPortal" in project:
-            project.append("SpatialPortal")
-
-    if len(project) == 1:
-        issue["project"] = project[0]
-        return True
-
-    # for issues that have 0 or multiple number of projects, print them out:
-    if len(project) == 0:
-        err = "NO PROJECT"
-                
-    if len(project) > 1:
-        err = "MULTIPLE PROJECTS ({})".format(project);
-
-    print '<tr><td>{}</td><td>{}</td><td><a href="https://code.google.com/p/ala/issues/detail?id={}">{}</a></td></tr>'.format(issue["ID"], err, issue["ID"], issue["Summary"])
-    return False
-
 def get_issue_details(issue):
 
-    if not extract_project_labels(issue):
-        return
-
+    # TODO: handle/report/log rubbish/non-existent issue ID (issue detail's page)
     page = requests.get("https://code.google.com/p/ala/issues/detail?id=" + issue["ID"])
     tree = html.fromstring(page.text)
 
@@ -243,20 +193,12 @@ def create_json(file_name, column_names):
     out = json.dumps( [row for row in csv_reader] )
 
     data = json.loads(out)
-    #print "number of issues found: %d" % len(data)
 
-    # simple report in HTML format into stdout
-    print '<html>'
-    print '<body>'
-    print '<table border="1">'
-    print '<tr><th>issue id</th><th>error description</th><th>link to issue</th></tr>'
-
+    counter = 0
     for issue in data:
+        print 'scraping issue id={}\t\t({}/{})'.format(issue["ID"], counter, len(data))
         get_issue_details(issue)
-
-    print '</table>'
-    print '</body>'
-    print '</html>'
+        counter = counter + 1
 
     out = json.dumps(data, sort_keys=True)
 
