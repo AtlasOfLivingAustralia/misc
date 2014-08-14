757128cb585e87dc7b64eb58cc24e4015179530c
mbohun <martin.bohun@gmail.com>
2014-08-14 15:01:23 +1000

    added back the project label processing and migration specific part-s of the code, this was moved here from the SCRAPE script, because it is exclusively MIGRATION specific and it has nothing to do with SCRAPE functionality

--- code.google-issues-2-github-issues/migrate-json-issues-to-github.py
+++ code.google-issues-2-github-issues/migrate-json-issues-to-github.py
@@ -1,16 +1,75 @@
 import sys
 import json
+import unicodedata
+
+def extract_project_labels(issue):
+    text = unicodedata.normalize('NFKD', issue["AllLabels"]).encode('ascii', 'ignore')
+    labels = text.strip().split(",")
+
+    project = []
+
+    # remove empty and non project label-s like Type-, Priority-
+    for label_text in labels:
+        label = label_text.strip()
+        if len(label) == 0:
+            continue
+
+        if label.find("Type") > -1:
+            continue
+
+        if label.find("Priority") > -1:
+            continue
+
+        if label.find("Milestone") > -1:
+            continue
+
+        project.append(label)
+
+    # TODO: do it properly/cleanly
+    # NORMALIZE - remove synonyms like SpatialPortal Spatial-Portal, replacing it with one label: SpatialPortal
+    # This applies to all Labels string: project names (synonyms, typos), priority (high, High, HIgh), etc.
+    if "Spatial-Portal" in project:
+        project.pop(project.index("Spatial-Portal"))
+        if not "SpatialPortal" in project:
+            project.append("SpatialPortal")
+
+    if len(project) == 1:
+        issue["project"] = project[0]
+        return True
+
+    # for issues that have 0 or multiple number of projects, print them out:
+    if len(project) == 0:
+        err = "NO PROJECT"
+
+    if len(project) > 1:
+        err = "MULTIPLE PROJECTS ({})".format(project);
+
+    print '<tr><td>{}</td><td>{}</td><td><a href="https://code.google.com/p/ala/issues/detail?id={}">{}</a></td></tr>'.format(issue["ID"], err, issue["ID"], issue["Summary"])
+    return False
 
 def migrate_issue(issue):
-    print 'migrating issue id={}'.format(issue["ID"])
+    if not extract_project_labels(issue):
+        return
+
+    print '<!-- migrating issue id={}\t\tto: {} -->'.format(issue["ID"], issue["project"])
 
 def migrate_json_issues(file_name):
     f = open(file_name[0], 'r')
     data = json.load(f)
     print 'len(data):{}'.format(len(data))
 
+    # simple report in HTML format into stdout
+    print '<html>'
+    print '<body>'
+    print '<table border="1">'
+    print '<tr><th>issue id</th><th>error description</th><th>link to issue</th></tr>'
+
     for issue in data:
         migrate_issue(issue)
+
+    print '</table>'
+    print '</body>'
+    print '</html>'
 
 if __name__=="__main__":
         migrate_json_issues(sys.argv[1:])
