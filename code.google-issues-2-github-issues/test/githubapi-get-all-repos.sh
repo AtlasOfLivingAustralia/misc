f3e72a40e014b9464dba7c430a38c9cf8b14eec4
mbohun <martin.bohun@gmail.com>
2014-10-02 17:36:49 +1000

    the github user (or organization) is now properly stored in a var

--- code.google-issues-2-github-issues/test/githubapi-get-all-repos.sh
+++ code.google-issues-2-github-issues/test/githubapi-get-all-repos.sh
@@ -5,18 +5,19 @@ if [ -z "$1" ]; then
     exit 1;
 fi
 
+GITHUB_USER_ORG=$1
 GITHUB_TOKEN=$2
 
 temp=`basename $0`
 TMPFILE=`mktemp /tmp/${temp}.XXXXXX` || exit 1
 
 # single page result-s (no pagination), have no Link: section, the grep result is empty
-last_page=`curl -s -I "https://api.github.com/users/$1/repos" -H "Authorization: token $GITHUB_TOKEN" | grep '^Link:'`
+last_page=`curl -s -I "https://api.github.com/users/$GITHUB_USER_ORG/repos" -H "Authorization: token $GITHUB_TOKEN" | grep '^Link:'`
 
 # does this result use pagination?
 if [ -z "$last_page" ]; then
     # no - this result has only one page
-    curl -s -i "https://api.github.com/users/$1/repos" -H "Authorization: token $GITHUB_TOKEN" | grep '"name":' >> $TMPFILE;
+    curl -s -i "https://api.github.com/users/$GITHUB_USER_ORG/repos" -H "Authorization: token $GITHUB_TOKEN" | grep '"name":' >> $TMPFILE;
 
 else
     # yes - this result is on multiple pages; extract the last_page number
@@ -24,7 +25,7 @@ else
 
     p=1
     while [ "$p" -le "$last_page" ]; do
-	curl -s -i "https://api.github.com/users/$1/repos?page=$p" -H "Authorization: token $GITHUB_TOKEN" | grep '"name":' >> $TMPFILE
+	curl -s -i "https://api.github.com/users/$GITHUB_USER_ORG/repos?page=$p" -H "Authorization: token $GITHUB_TOKEN" | grep '"name":' >> $TMPFILE
 	p=$(($p + 1))
     done
 fi
